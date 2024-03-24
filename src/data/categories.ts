9ac27a4d23dcd0c5af97dfe608112719f05bdcd0
puppe1990 <matheus.puppe90@hotmail.com>
2024-03-24 20:45:57 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,2 @@
+// List of categories for blog posts
+export const CATEGORIES = ['Category 1', 'Como Plantar Frutas', 'Jardinagem'] as const
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'DanielCG', // Site author
+	title: 'Blog Semear Saber', // Site title.
+	description: 'Blog focado em plantação de legumes, frutas e vegetais', // Description to display in the meta tags
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	shareMessage: 'Share this post', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
