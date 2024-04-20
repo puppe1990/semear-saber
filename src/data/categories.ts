540593730c624a80656e30e1cb4b5b6c627278df
puppe1990 <matheus.puppe90@hotmail.com>
2024-04-20 18:19:32 -0300

    new article

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Horticultura', 'Jardinagem Doméstica', 'Jardinagem Interior', 'Como Plantar Frutas', 'Jardinagem', 'Saúde e Bem-Estar'] as const
+export const CATEGORIES = ['Horticultura', 'Jardinagem Doméstica', 'Jardinagem Interior', 'Como Plantar Frutas', 'Jardinagem', 'Saúde e Bem-Estar', 'Jardinagem e Plantas'] as const
